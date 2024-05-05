b7f2a862db9f387394b785d042de69cf22e2aae1
ygeuhijk <gs1jonesdil@hotmail.com>
2024-05-05 11:06:48 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const initial = (arr) => arr.slice(0, -1);
+function combinationSum4(nums, target) {
+  const dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= target; i++) {
+    for (const num of nums) {
+      if (i >= num) {
+        dp[i] += dp[i - num];
+      }
+    }
+  }
+  return dp[target];
+}
