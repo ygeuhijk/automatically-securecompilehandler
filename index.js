9b770b36e02af6c9bd708f6e1e96c51c4c1e7242
ygeuhijk <gs1jonesdil@hotmail.com>
2024-05-06 10:38:10 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function combinationSum4(nums, target) {
-  const dp = new Array(target + 1).fill(0);
-  dp[0] = 1;
-  for (let i = 1; i <= target; i++) {
-    for (const num of nums) {
-      if (i >= num) {
-        dp[i] += dp[i - num];
-      }
-    }
-  }
-  return dp[target];
-}
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
