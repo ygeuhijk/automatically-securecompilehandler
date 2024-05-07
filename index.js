71050d8684f5ef3767ac34c2b378fba99a08ca09
ygeuhijk <gs1jonesdil@hotmail.com>
2024-05-07 08:17:10 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const reverseWords = (str) => str.split(" ").reverse().join(" ");
+function productExceptSelf(nums) {
+  const n = nums.length;
+  const result = new Array(n).fill(1);
+  let product = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  product = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  return result;
+}
