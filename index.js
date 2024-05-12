c5c183b8599e410f13e2790ee6f5cce615823304
ygeuhijk <gs1jonesdil@hotmail.com>
2024-05-13 02:05:28 +0700

    start

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const remove = (arr, func) =>
-  Array.isArray(arr)
-    ? arr.filter(func).reduce((acc, val) => {
-        arr.splice(arr.indexOf(val), 1);
-        return acc.concat(val);
-      }, [])
-    : [];
+function maxArea(height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    maxArea = Math.max(
+      maxArea,
+      Math.min(height[left], height[right]) * (right - left),
+    );
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+  return maxArea;
+}
