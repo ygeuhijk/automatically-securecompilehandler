b674f9ea9d185f4b7b25591ab3f5aeeb6b1775ed
ygeuhijk <gs1jonesdil@hotmail.com>
2024-05-13 08:47:00 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function maxArea(height) {
-  let maxArea = 0;
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left),
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
+const gcdRecursive = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  return maxArea;
-}
+  return gcdRecursive(b, a % b);
+};
